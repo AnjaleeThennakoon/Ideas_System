e51b4a9ae515517fb3144de03e29eea33e5457b8
Anjalika Rukshani <anjalikarukshani51@gmail.com>
2026-08-31 14:45:09 +0530

    feat: add relationships for Step and Idea models

--- app/Models/idea.php
+++ app/Models/idea.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class idea extends Model
 {
@@ -20,8 +21,8 @@ class idea extends Model
     {
          return $this ->belongsTo(User::class);
     }
-    public function steps():BelongsTo
+    public function steps():HasMany
     {
-         //  return $this ->belongsTo(User::class);
+        return $this->hasmany(step::class);
     }
 }
